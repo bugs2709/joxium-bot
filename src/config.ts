0421e63d28c74c3a6197d3a90af4d23d3dff5148
bugs2709 <ramirezibarraesau@gmail.com>
2026-07-02 13:32:42 -0700

    Add env debug logs

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,5 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 
-export const config = {
-  token: process.env.DISCORD_TOKEN!,
-  clientId: process.env.CLIENT_ID!,
-};
-
-if (!config.token) {
-  throw new Error("Missing DISCORD_TOKEN in .env");
-}
-
-if (!config.clientId) {
-  throw new Error("Missing CLIENT_ID in .env");
-}
+console.log("ENV CHECK:", Object.keys(process.env).includes("DISCORD_TOKEN"));
+console.log("TOKEN LENGTH:", process.env.DISCORD_TOKEN?.length);
